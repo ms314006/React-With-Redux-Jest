a0cb4b6fee378c7a82ac17c94f797fc11e588674
GQSM <ms314006@hotmail.com>
2019-04-22 22:33:29 +0800

    [CHANGE] 將 Component 重新放置到 src/component 中

--- src/component/Counter/Counter.js
+++ src/component/Counter/Counter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import addCounter from '../actions/Counter';
+import addCounter from '../../actions/Counter';
 
 const Counter = (props) => {
   const { count, addCount, } = props;
